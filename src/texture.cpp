5f88d267e9ea4b2a19200c375607fa24b2be3808
xordog <xordog@gmail.com>
2023-12-17 00:55:56 +0900

    Use GL_CLAMP_TO_EDGE for texture

--- src/texture.cpp
+++ src/texture.cpp
@@ -197,8 +197,8 @@ namespace Rays
 		if (glIsTexture(self->id) == GL_FALSE)
 			opengl_error(__FILE__, __LINE__, "failed to create texture.");
 
-		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);//GL_LINEAR);
 
