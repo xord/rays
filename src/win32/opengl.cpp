e1d47fa1c26d29dbd79f170c2131d581cf462c8a
xordog <xordog@gmail.com>
2024-04-08 00:33:52 +0900

    Add empty implementations throw NotImplementedError

---
+++ src/win32/opengl.cpp
@@ -0,0 +1,31 @@
+#include "../opengl.h"
+
+
+#include "rays/exception.h"
+
+
+namespace Rays
+{
+
+
+	void
+	OpenGL_set_context (Context context)
+	{
+		not_implemented_error(__FILE__, __LINE__);
+	}
+
+	Context
+	OpenGL_get_context ()
+	{
+		not_implemented_error(__FILE__, __LINE__);
+	}
+
+
+	Context
+	get_offscreen_context ()
+	{
+		not_implemented_error(__FILE__, __LINE__);
+	}
+
+
+}// Rays
